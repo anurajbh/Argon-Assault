043153acb5a11beb74b333a66dd4a7d8fa9d3450
anurajbh <anurajbh@users.noreply.github.com>
2018-11-19 10:34:36 -0500

    Added splash screen

---
+++ Assets/Scripts/LevelLoad.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoad : MonoBehaviour
+{
+
+    Scene scene;
+    int sceneIndex;
+	// Use this for initialization
+	void Start ()
+    {
+        scene = SceneManager.GetActiveScene();
+        sceneIndex = scene.buildIndex;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        DontDestroyOnLoad(gameObject);
+		if(Input.GetKeyDown(KeyCode.Return))
+        {
+            LoadTheNextLevel(sceneIndex);
+        }
+	}
+
+    private void LoadTheNextLevel(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex + 1);
+    }
+}
