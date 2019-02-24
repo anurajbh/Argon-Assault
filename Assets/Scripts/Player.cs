f510d4f322ca6ca8002f2449cea152d2147cd065
anurajbh <anurajbh@users.noreply.github.com>
2019-02-24 18:39:35 -0500

    Bullet, Explosion Particles, Terrain restructuring

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,11 +16,14 @@ public class Player : MonoBehaviour
     float yThrow;
 
     //Rotation parameters
-    [SerializeField] float pitchFactor = -5f;
-    [SerializeField] float yawFactor = 5f;
-    [SerializeField] float rollFactor = -5f;
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float controlPitchFactor = -20f;
+    [SerializeField] float positionYawFactor = 5f;
+    [SerializeField] float controlRollFactor = -20f;
 
-   // Use this for initialization
+    
+
+    // Use this for initialization
     void Start ()
     {
 		
@@ -37,15 +40,17 @@ public class Player : MonoBehaviour
 
     private void ProcessRotation()
     {
-        float pitch = transform.localPosition.y * pitchFactor + yThrow;
-        float yaw = transform.localPosition.x * yawFactor + xThrow;
-        float roll = xThrow * rollFactor;
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;//pitch is coupled to both control throw and position
+        float yaw = transform.localPosition.x * positionYawFactor;//yaw is coupled to position
+        float roll = xThrow * controlRollFactor;//roll is coupled to control throw
         transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 
     private float MoveTheShipX()
     {
-        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");//crossplatform input
         float xOffsetThisFrame = xSpeed * xThrow * Time.deltaTime;
         float rawXPos = transform.localPosition.x + xOffsetThisFrame;
         float xPos = Mathf.Clamp(rawXPos, -xRange, +xRange);
@@ -54,7 +59,7 @@ public class Player : MonoBehaviour
 
     private float MoveTheShipY()
     {
-        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");//crossplatform input
         float yOffsetThisFrame = ySpeed * yThrow * Time.deltaTime;
         float rawYPos = transform.localPosition.y + yOffsetThisFrame;
         float yPos = Mathf.Clamp(rawYPos, -yRange, +yRange);
