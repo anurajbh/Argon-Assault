8fb0690942bebd04c5ce1d0df4c1c652868df10b
anurajbh <anurajbh@users.noreply.github.com>
2019-05-13 23:57:08 -0400

    TODO- Add victory sound, modify end credit page

---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+    Scene CurrentScene;
+    int sceneIndex;
+    private void Awake()
+    {
+        CurrentScene = SceneManager.GetActiveScene();
+        sceneIndex = CurrentScene.buildIndex;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(sceneIndex+1);
+    }
+}
